423fae407e1c034db85508c1598a437b22253075
TakaYuPP <yutotaka24@gmail.com>
2025-11-01 15:45:47 +0900

    feat: registered Qusino smart contract

---
+++ src/contracts/Qusino.h
@@ -0,0 +1,28 @@
+using namespace QPI;
+
+struct QUSINO2
+{
+};
+
+struct QUSINO : public ContractBase
+{
+	REGISTER_USER_FUNCTIONS_AND_PROCEDURES()
+	{
+	}
+
+	INITIALIZE()
+	{
+	}
+
+	BEGIN_EPOCH()
+	{
+	}
+
+    END_EPOCH()
+    {
+    }
+
+    PRE_ACQUIRE_SHARES()
+    {
+    }
+};
